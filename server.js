82dbef09b3134822fd05b8215e1a517cc292ab83
Max Boettcher <mvboettcher@gmail.com>
2021-02-28 22:27:17 -0500

    adds package (cors)

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const listings = require('./routes/listings')
 
 const app = express()
+
+app.use(cors())
 
 // Mount routers
 app.use('/api/listings', listings)
